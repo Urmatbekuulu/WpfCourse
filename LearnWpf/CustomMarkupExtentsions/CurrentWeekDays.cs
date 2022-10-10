54de81c9e12ab88e4ce897a33e0923d82b3ffa9c
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-10-10 11:29:46 +0600

    Xaml Resources Task
    Create two ResourceDictionary files with different ControlTemplates for Button. These templates should have the same key. For example:
    
    ResourceDictionary1.xaml:
    
    ...
    <ControlTemplate x:Key="your_key_here" TargetType="Button">
     <Grid>
      <Rectangle Fill="Red" />
      <ContentPresenter VerticalAlignment="Center" HorizontalAlignment="Center" />
     </Grid>
    </ControlTemplate>
    ...
    ResourceDictionary2.xaml:
    
    ...
    <ControlTemplate x:Key="your_key_here" TargetType="Button">
     <Grid>
      <Rectangle Fill="Green" />
      <ContentPresenter VerticalAlignment="Center" HorizontalAlignment="Center" />
     </Grid>
    </ControlTemplate>
    ...
    
    To apply one of these resource dictionaries at the Application level, add it to Application.Resources in the App.xaml file as a merged dictionary.
    
    Add a button to your window and set it's template using StaticResource or DynamicResource (choose wisely :)) Switch between resource dictionaries on a button click. For this, you will need to remove the old merged dictionary and add another merged dictionary.

--- LearnWpf/CustomMarkupExtentsions/CurrentWeekDays.cs
+++ LearnWpf/CustomMarkupExtentsions/CurrentWeekDays.cs
@@ -2,15 +2,14 @@
 using System.Collections.ObjectModel;
 using System.Windows.Markup;
 
-namespace LearnXAML.CustomMarkupExtentsions; 
-
+namespace LearnXAML.CustomMarkupExtentsions;
 public class CurrentWeekDays:MarkupExtension {
     public string Format { get; set; } = "yyyy MMMM dd";
     public DayOfWeek StartDayOfWeek { get; set; } = DayOfWeek.Monday;
    
     public override object ProvideValue(IServiceProvider serviceProvider) {
-        
         var weekDays = new Collection<string>();
+        
         var today = DateTime.Now.DayOfWeek;
         var startDay = DateTime.Today.AddDays((int)StartDayOfWeek-(int)today);
         var endDay = startDay.AddDays(Enum.GetNames<DayOfWeek>().Length);
