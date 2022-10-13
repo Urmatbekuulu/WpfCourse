44779a6fcb9da5fe03d2ecfc33a5e1979b87254f
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-10-13 12:22:17 +0600

    task06
    Implement all three tasks from the previous point by binding properties in C# code

--- LearnWpf/MainWindow.xaml.cs
+++ LearnWpf/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,6 +21,24 @@ namespace LearnXAML {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            //task1
+            Binding binding = new Binding(nameof(textBox.Text));
+            binding.Source = textBox;
+            textBlcok.SetBinding(TextBlock.TextProperty,binding);
+
+            Binding editableBinding = new Binding(nameof(checkBox.IsChecked)) {
+                Source = checkBox,
+            };
+            textBox.SetBinding(TextBoxBase.IsReadOnlyProperty, editableBinding);
+            //task2
+            Binding selfBinding = new Binding(nameof(border.Width));
+            selfBinding.RelativeSource = RelativeSource.Self;
+            border.SetBinding(Border.HeightProperty, selfBinding);
+            //task3
+            Binding titleBinding = new Binding(nameof(titleEditor.Text));
+            titleBinding.Source = titleEditor;
+            this.SetBinding(Window.TitleProperty, titleBinding);
+
         }
     }
 }
