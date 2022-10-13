718e771bf77245e351e615d2cf51a7420e95122b
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-10-13 14:31:25 +0600

    Converters
    Implement a converter that will invert the incoming brush. Bind TextBlock.Foreground to TextBlock.Background via this converter.

---
+++ LearnWpf/CustomConverters/ForegroundToBackgroundConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using static System.Windows.Media.Brushes;
+using static System.Windows.Media.Colors;
+
+namespace LearnXAML.CustomConverters; 
+
+public class ForegroundToBackgroundConverter:IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        var foreground = (SolidColorBrush)value;
+        var background = new SolidColorBrush(Color.FromRgb(
+            (byte)(((int)foreground.Color.R+128)%255)
+            ,(byte)(((int)foreground.Color.R+128)%255)
+            ,(byte)(((int)foreground.Color.R+128)%255)
+            ));
+       
+        return background;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        return null;
+    }
+}
