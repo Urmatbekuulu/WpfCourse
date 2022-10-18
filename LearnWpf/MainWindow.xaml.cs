07110eb5b5cf0e49b398641e3f040d09e2ddfc2c
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-10-18 20:57:59 +0600

    task12
    Create a project with the Grid element that contains various controls: TextBoxes, Buttons, CheckBoxes, etc. Catch the moment when the Esc key is pressed on any of these controls without directly subscribing to events on each control. When the Esc key is pressed - disable all controls at once. There should be a way to enable them again.

--- LearnWpf/MainWindow.xaml.cs
+++ LearnWpf/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LearnXAML {
     /// <summary>
@@ -21,5 +9,13 @@ namespace LearnXAML {
         public MainWindow() {
             InitializeComponent();
         }
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e) {
+            if (e.Key != Key.Escape) return;
+
+            checkBox.IsEnabled = !checkBox.IsEnabled;
+            button.IsEnabled = !button.IsEnabled;
+            textBox.IsEnabled = !textBox.IsEnabled;
+        }
+        
     }
 }
