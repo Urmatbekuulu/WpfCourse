2d54665cef8727c66d4c75c94f30444f615e3d2c
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-10-21 19:26:27 +0600

    Task 15 is finished
    Create a simple View which shows a list of persons and allows editing each person's property.
    
    For this, you need to create a Person object and fill ListBox with this object's instances. Than, override ListBox's ItemTemplate and place several elements to display all the object's properties in a row. After that, define several editors at the bottom of the View which allows you to edit a person's properties. ListBox should reflect each change as well.
    
    Finally, use Triggers, DataTriggers or MultiTriggers in ItemTemplate to colorize items in ListBox based on a person's data and selection state. For example, colorize persons whose Age is greater than 50. Note that colors for selected and non-selected items should be different.

---
+++ LearnWpf/AgeToColorConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LearnXAML; 
+
+public class AgeToColorConverter:IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        return (int)value%2==0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
--- LearnWpf/MainWindow.xaml.cs
+++ LearnWpf/MainWindow.xaml.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LearnXAML {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public ObservableCollection<Person> People { get; set; } = new ();
         public MainWindow() {
             InitializeComponent();
+            for (int i = 0; i < 100; i++) {
+                People.Add(new Person {
+                    FirstName = $"FirstName#{i}",
+                    LastName = $"LastName#{i}",
+                    Age = i
+                });
+            }
+
+            this.DataContext = this;
         }
+    }
+    public class Person {
+        public int Age { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        
     }
 }
