7812bff721ffac6b921f93fbba9ae7e60eff853d
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-10-31 09:42:01 +0600

    task 23 is finished
    1) Create a UserControl that is an analog of the standard WPF GroupBox control. Add two properties: Caption and Content. Add this control to a window and allow editing its caption.
    
    2) Inside the Window class register two Dependency properties: TestString(string) and MaxLength(int). Put two TextBox controls and bind them to the TestString and MaxLength properties. For the MaxLength and TestString properties implement callbacks so that:
    
    MaxLength is >= 0;
    The length of TestString is <= MaxLength, or TestString is of any length if MaxLength = 0.
    3) Put a ToggleButton control. Use DependencyPropertyDescriptor to subscribe to changing its IsChecked property and perform some actions (show a MessageBox with the current button’s state) when the button’s “toggled” state is changed.

---
+++ LearnWpf/GoupBoxUserControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LearnXAML; 
+
+public partial class GridBoxUserControl : UserControl {
+    public GridBoxUserControl() {
+        InitializeComponent();
+    }
+        
+    public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register(
+        nameof(Caption), typeof(object), typeof(GridBoxUserControl), new PropertyMetadata(default(object)));
+
+    public object Caption {
+        get { return (object)GetValue(CaptionProperty); }
+        set { SetValue(CaptionProperty, value); }
+    }
+    public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+        nameof(Content), typeof(object), typeof(GridBoxUserControl), new PropertyMetadata(default(object)));
+
+    public object Content {
+        get { return (object)GetValue(ContentProperty); }
+        set { SetValue(ContentProperty, value); }
+    }
+}
--- LearnWpf/MainWindow.xaml.cs
+++ LearnWpf/MainWindow.xaml.cs
@@ -1,25 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace LearnXAML {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
+        
+        public PropertyDescriptor PropertyDescriptor { get; set; }
+        public string TestString {
+            get { return (string)GetValue(TestStringProperty); }
+            set { SetValue(TestStringProperty, value); }
         }
+        public static readonly DependencyProperty TestStringProperty = DependencyProperty.Register(
+            nameof(TestString), typeof(string)
+            , typeof(MainWindow),
+            typeMetadata: new FrameworkPropertyMetadata(
+                defaultValue: default(string),
+                flags: FrameworkPropertyMetadataOptions.AffectsMeasure
+                ,propertyChangedCallback:new PropertyChangedCallback(OnTestStringChanged)
+                ,coerceValueCallback:new CoerceValueCallback(CoerceStringTesting)
+                
+            )
+        );
+        public int MaxLength {
+            get { return (int)GetValue(MaxLengthProperty); }
+            set { SetValue(MaxLengthProperty, value);}
+        }
+        
+        public static readonly DependencyProperty MaxLengthProperty = DependencyProperty.Register(
+            name: nameof(MaxLength)
+            ,propertyType: typeof(int)
+            ,ownerType: typeof(MainWindow)
+            , typeMetadata: new FrameworkPropertyMetadata(
+                defaultValue: default(int),
+                flags: FrameworkPropertyMetadataOptions.AffectsMeasure),
+            validateValueCallback: new ValidateValueCallback(IsValidMaxLength));
+        
+        public MainWindow() {
+            
+            InitializeComponent();
+            
+            PropertyDescriptor =
+                DependencyPropertyDescriptor.FromProperty(ToggleButton.IsCheckedProperty, typeof(ToggleButton));
+            PropertyDescriptor.AddValueChanged(toggleButton, OnIsCheckedPropertyChanged);
+        }
+       
+        public EventHandler OnIsCheckedPropertyChanged = (object source,EventArgs args) => {
+            var toggleButSource = source as ToggleButton;
+            MessageBox.Show($"{(toggleButSource.IsChecked.Value?"The button's state is pressed" :"The toggle button's state is normal")}");
+        };
+
+        private static void OnTestStringChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args) {
+            dependencyObject.CoerceValue(TestStringProperty);
+        }
+        private static object CoerceStringTesting(DependencyObject depObj, object value) {
+            MainWindow mainWindow = (MainWindow)depObj;
+            string currentString = (string)value;
+            if (mainWindow.MaxLength == 0)
+                return currentString;
+            if (mainWindow.MaxLength < currentString.Length)
+                return mainWindow.TestString.Substring(0,mainWindow.MaxLength);
+            return currentString;
+        }
+        public static bool IsValidMaxLength(object value) {
+            return (int)value >= 0;
+        }
+        protected override void OnClosing(CancelEventArgs e) {
+            base.OnClosing(e);
+            PropertyDescriptor.RemoveValueChanged(toggleButton, OnIsCheckedPropertyChanged);
+        }
+        
     }
 }
