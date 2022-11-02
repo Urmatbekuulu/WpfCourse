a98e6fd74b7977efec9c6ffa184bb99c7683f9ec
Raatbek <raatbekurmatbekuulu@gmail.com>
2022-11-02 10:16:31 +0600

    task 26 is finished
    1) Create a custom panel that arranges child elements in a circle. The panel should have the Radius property which allows setting the circle's radius and calculates positions of items according to the radius.
    
    2) Create another custom panel that arranges child elements in two modes: horizontally or vertically. The panel should have a property which specifies offsets between items.

---
+++ LearnWpf/CirceCustomPanel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LearnXAML; 
+
+public class CircleCustomPanel:Panel {
+    public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register(
+        nameof(Radius), typeof(double), typeof(CircleCustomPanel), new PropertyMetadata(default(double)));
+
+    public double Radius {
+        get { return (double)GetValue(RadiusProperty); }
+        set { SetValue(RadiusProperty, value); }
+    }
+    public CircleCustomPanel():base() {
+        
+    }
+    protected override Size MeasureOverride(Size availableSize) {
+        Size panelDesiredSize = new Size();
+        foreach (UIElement child in InternalChildren) {
+            child.Measure(availableSize);
+            panelDesiredSize = child.DesiredSize;
+        }
+        return panelDesiredSize;
+    }
+    protected override Size ArrangeOverride(Size finalSize) {
+       
+        double angle = Math.PI/2,r = Radius;
+        double deltaAngle = 2*Math.PI/ InternalChildren.Count;
+        double x, y;
+        foreach (UIElement child in InternalChildren) {
+            y = r - r * Math.Sin(angle);
+            x = r + r * Math.Cos(angle);
+            angle += deltaAngle;
+            child.Arrange(new Rect(new Point(x,y),child.DesiredSize)); 
+        }
+        return finalSize;
+    }
+}
---
+++ LearnWpf/CustomStackPanel.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LearnXAML; 
+
+public class CustomStackPanel:Panel {
+    public static readonly DependencyProperty OffsetProperty = DependencyProperty.Register(
+        nameof(Offset), typeof(double), typeof(CustomStackPanel), new PropertyMetadata(default(double)));
+
+    public double Offset {
+        get { return (double)GetValue(OffsetProperty); }
+        set { SetValue(OffsetProperty, value); }
+    }   
+
+    public static readonly DependencyProperty IsHorizontalModeProperty = DependencyProperty.Register(
+        nameof(IsHorizontalMode), typeof(bool), typeof(CustomStackPanel), new PropertyMetadata(default(bool)));
+
+    public bool IsHorizontalMode {
+        get { return (bool)GetValue(IsHorizontalModeProperty); }
+        set { SetValue(IsHorizontalModeProperty, value); }
+    }
+    public CustomStackPanel() : base() {
+        
+    }
+
+    protected override Size MeasureOverride(Size availableSize) {
+        Size panelDesiredSize = new Size();
+        foreach (UIElement child in InternalChildren) {
+            child.Measure(availableSize);
+            panelDesiredSize = child.DesiredSize;
+        }
+        return panelDesiredSize;
+    }
+
+    protected override Size ArrangeOverride(Size finalSize) {
+        double x=0, y=0;
+        foreach (UIElement child in InternalChildren) {
+            child.Arrange(new Rect(new Point(x,y),child.DesiredSize));
+            if (IsHorizontalMode) {
+                x += child.DesiredSize.Width + Offset;
+            }
+            else {
+                y += child.DesiredSize.Height + Offset;
+            }
+        }
+        return finalSize;
+    }
+}
